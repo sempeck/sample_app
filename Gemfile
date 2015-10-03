f520d0b15c4b64dcad851d3aca1e4a6cc2326203
sempeck <jarek.sepek@mediorama.pl>
2015-10-03 22:57:52 +0200

    user index

--- Gemfile
+++ Gemfile
@@ -8,9 +8,12 @@ gem 'gravatar_image_tag', '0.1.0'
 
 gem 'protected_attributes'
 
+# gem 'will_paginate', '3.0.pre2'
+
 group :development do
 gem 'rspec-rails'#, '2.6.1.beta1'
 # gem 'rspec-rails'#, '2.0.1'
+gem 'faker', '0.3.1'
 end
 group :test do
 # gem 'rspec', '2.6'	
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
         render 'new'
       else
         sign_in user
-        redirect_to user
+        redirect_back_or user
       end
     end
 
@@ -24,4 +24,3 @@ class SessionsController < ApplicationController
     end
 
 end
-
